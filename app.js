6ac660ccbf79064f61f2b799525e329c4e23b3e6
NumberJuan1 <juanbruno18jb@gmail.com>
2023-04-19 09:34:42 -0300

    Actu Final 19/4

--- app.js
+++ app.js
@@ -21,8 +21,30 @@ function mostrar(){
     document.getElementById('formu').style.display = 'block';
 }
 
-
-
+$(document).ready(function () {
+    $("#btnEnviar").click(function () {
+         var nombre = $("#nombre2").val();
+         var correo = $("#correo").val();
+         var mensaje = $("#mensaje").val();
+             $.ajax({
+                 url: "https://reqres.in/api/users",
+                 type: "POST",
+                 data: {
+                    name: nombre,
+                    mail: correo,
+                    mens: mensaje,
+                  },
+                 success: function (result) {
+                 console.log(result);
+                        $("#respuestaConsola").html(JSON.stringify(result));
+                        $("#respuestaHTML").html("<p>Hola " + result.name + ", gracias por tu respuesta, se envio un descuento a tu mail : " + result.mail +"</p>");
+                    },
+                    error: function (xhr, status, error) {
+                        alert("Ha ocurrido un error: " + error);
+                    }
+                });
+            });
+  });
   
 
  
